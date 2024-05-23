1bd84308dbe8a3ba2c59c52accdd748a59a49570
PChaebin <pcd0299@gmail.com>
2024-05-24 00:21:04 +0900

    24.05.24 Update
    - 만드라 이동 구현

--- Wiheoman Forest/Assets/_Scripts/Enemy/MandalaController.cs
+++ Wiheoman Forest/Assets/_Scripts/Enemy/MandalaController.cs
@@ -5,38 +5,97 @@ using UnityEngine;
 
 public class MandalaController : MonoBehaviour
 {
-    [SerializeField] private float sensingRange;
-    [SerializeField] private float mandalaJump;
+    [System.Serializable]
+    public enum MandalaType
+    {
+        Herb,
+        Explosion
+    }
+
+    [SerializeField] private MandalaType type;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float sensingRange;
+    [SerializeField] private float mandalaJumpSpeed;
+    [SerializeField] private float mandalaMovSpeed;
 
     private bool isPerceive;
+    private bool isGround = false;
     private Rigidbody mandalaRb;
-    private Vector3 mandalaPos;
+    private GameObject player;
+    private Rigidbody playerRb;
+    private Collider col;
+    private Vector3 herbMandalaVec;
+
+    private float groundPos = 1.0f;
+    private float mandalaJumpMax = 1.5f;
 
     void Start()
     {
-        if (mandalaRb == null)
+        if (!mandalaRb)
         {
             mandalaRb = GetComponent<Rigidbody>();
         }
         mandalaRb.useGravity = false;
+        col = GetComponent<Collider>();
+
+        //
+        player = GameObject.Find("Player");
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // CheckSensingRange();
-
-        if (Physics.CheckSphere(transform.position, sensingRange, playerLayer))
+        if (!isPerceive && Physics.CheckSphere(transform.position, sensingRange, playerLayer))
         {
-            mandalaRb.AddForce(Vector3.up * mandalaJump, ForceMode.Impulse);
-            mandalaRb.useGravity = true;
-            isPerceive = false;
+            isPerceive = true;            
+            StartCoroutine(upGround());
         }
+
+        if (isGround && isPerceive)
+        {
+            switch (type)
+            {
+                case MandalaType.Herb:
+                    herbMandalaVec = (transform.position - playerRb.position).normalized;
+                    break;
+                case MandalaType.Explosion:
+                    herbMandalaVec = (playerRb.position - transform.position).normalized;
+                    break;
+            }
+        }
+        transform.position += herbMandalaVec * mandalaMovSpeed * Time.deltaTime;
+
+        /*if ((transform.position - player.transform.position).magnitude < 1.0f)
+        {
+            //폭발 코드
+            gameObject.SetActive(false);
+        }*/
     }
 
-    private void CheckSensingRange()
+    /// <summary>
+    /// 땅에 심어져있던 만드라가 땅위로 올라오는 IEnumerator
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator upGround() 
     {
-        isPerceive = Physics.CheckSphere(transform.position, sensingRange, playerLayer);
+        while (transform.position.y <= mandalaJumpMax)
+        {
+            mandalaRb.AddForce(Vector3.up * mandalaJumpSpeed * Time.deltaTime, ForceMode.Impulse);
+            yield return null;
+        }
+        mandalaRb.useGravity = true;
+
+        col.isTrigger = false;
+        isGround = true;
+    }
+
+    // 안됨..
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnDrawGizmos()
