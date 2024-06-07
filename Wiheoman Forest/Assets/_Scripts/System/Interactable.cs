32ca39a866cb40c5e168c4720141d083845b121f
yujini1121 <yujini1121@naver.com>
2024-06-07 18:31:26 +0900

    24.06.07 Update

---
+++ Wiheoman Forest/Assets/_Scripts/System/Interactable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
