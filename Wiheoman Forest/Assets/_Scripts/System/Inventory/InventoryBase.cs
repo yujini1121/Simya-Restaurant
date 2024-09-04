5da56f8e851a3da81e205b6ab1dc61e05eb560ce
yujini <yujini1121@naver.com>
2024-09-05 06:50:07 +0900

    Update InventoryBase

--- Wiheoman Forest/Assets/_Scripts/System/Inventory/InventoryBase.cs
+++ Wiheoman Forest/Assets/_Scripts/System/Inventory/InventoryBase.cs
@@ -3,12 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum InventoryType
+{
+    PlayerInventory,
+    ChestInventory
+}
 
 public class InventoryBase : MonoBehaviour
 {
+    //[SerializeField] InventoryType type;
 
-    private void Start()
+
+    public void AddItemSlot(ItemAttribute item, int count = 1)
     {
-        //UI_Element.instance.
+        string itemName = item.ToString();
+        int itemCount = count;
     }
+
+    //private void AcquireItem(ItemAttribute item, int count = 1)
+    //{
+    //    if (item.CanOverlap)
+    //    {
+    //        for (int i = 0; i < slots.Length; i++)
+    //        {
+    //            if (slots[i].Item != null && slots[i].Item.ItemID == item.ItemID)
+    //            {
+    //                slots[i].ItemCountUpdate(count);
+    //                return;
+    //            }
+    //        }
+    //    }
+
+    //    for (int i = 0; i < slots.Length; i++)
+    //    {
+    //        if (slots[i].Item == null)
+    //        {
+    //            slots[i].AddItemSlot(item, count);
+
+    //            if (slots[i].gameObject.activeSelf == false)
+    //            {
+    //                curSlotCount++;
+    //            }
+    //            return;
+    //        }
+    //    }
+    //}
 }
