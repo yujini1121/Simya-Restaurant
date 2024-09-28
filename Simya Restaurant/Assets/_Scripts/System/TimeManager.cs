f7d9fa9c95e8e73a19f6899fe57ff2a8d5ff9daa
yujini <yujini1121@naver.com>
2024-09-28 19:46:05 +0900

    Time Manager Update
    - 수정 중

--- Simya Restaurant/Assets/_Scripts/System/TimeManager.cs
+++ Simya Restaurant/Assets/_Scripts/System/TimeManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -12,165 +11,208 @@ public enum TimeOfDay
 
 public class TimeManager : MonoBehaviour
 {
+    #region 아 다시 짜야지
+    //[SerializeField] private TimeOfDay current;
+    //[SerializeField] private TextMeshProUGUI timeText;
+
+    //[Space(20)]
+    //[Tooltip("현실 시간과 게임 시간 간의 변환 비율")]
+    //[SerializeField] private float realTimeToGameTimeMultiplier;
+
+    //[SerializeField] private float initGameTime;
+    //[SerializeField] private float sunriseHour;
+    //[SerializeField] private float sunsetHour;
+
+    //private DateTime currentTime;
+    //private TimeSpan dayTimeSpan;
+    //private TimeSpan nightTimeSpan;
+
+    //[SerializeField] private Color dayAmbientLight;
+    //[SerializeField] private Color nightAmbientLight;
+    //[SerializeField] private Color dawnAmbientLight;
+
+    //[SerializeField] private AnimationCurve lightChangeCurve;
+
+
+    //private void Start()
+    //{
+    //    currentTime = DateTime.Now.Date + TimeSpan.FromHours(initGameTime);
+
+    //    dayTimeSpan = TimeSpan.FromHours(sunriseHour);
+    //    nightTimeSpan = TimeSpan.FromHours(sunsetHour);
+    //}
+
+    //private void Update()
+    //{
+    //    if (current == TimeOfDay.Night)
+    //    {
+    //        timeText.enabled = true;
+    //        UpdateTime();
+    //    }
+    //    else
+    //    {
+    //        timeText.enabled = false;
+    //    }
+    //}
+
+    //private void UpdateTime()
+    //{
+    //    currentTime = currentTime.AddSeconds(Time.deltaTime * realTimeToGameTimeMultiplier);
+
+    //    if (timeText != null)
+    //    {
+    //        timeText.text = currentTime.ToString("HH:mm");
+    //    }
+    //}
+
+    ///// <summary>
+    ///// case 1. 새벽 -> 낮 ///
+    ///// case 2. 낮 -> 밤
+    ///// </summary>
+    //private void SkipToNextTime(TimeOfDay current)
+    //{
+    //    if      (current == TimeOfDay.Dawn)
+    //    {
+
+    //    }
+    //    else if (current == TimeOfDay.Day)
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("SkipToNextTime() - Not Found Current Time");
+    //        return;
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 밤 -> 새벽 
+    ///// </summary>
+    //private void DurationToNextTime(TimeOfDay current)
+    //{
+    //    if (current == TimeOfDay.Night)
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("DurationToNextTime() - Not Found Current Time");
+    //        return;
+    //    }
+    //}
+    #endregion
+    //IEnumerator ChangeNextTime()
+    //{
+    //    switch (current)
+    //    {
+    //        case TimeOfDay.Dawn:    DawnToDay();    break;
+    //        case TimeOfDay.Day:     DayToNight();   break;
+    //        case TimeOfDay.Night:   NightToDawn();  break;
+    //    }
+
+    //    yield return null;
+    //}
+
+    [Header("Current")]
     [SerializeField] private TimeOfDay current;
-    [SerializeField] private TextMeshProUGUI timeText;
 
-    [Space(20)]
+    [SerializeField] private Light light;
+    [SerializeField] private Color filter;
+    [SerializeField] private float temperature;
+    [SerializeField] private float intensity;
+
+    [Header("Night To Dawn")]
     [Tooltip("현실 시간과 게임 시간 간의 변환 비율")]
+    [SerializeField] private TextMeshProUGUI timeText;
     [SerializeField] private float realTimeToGameTimeMultiplier;
-
-    [SerializeField] private float initGameTime;
-    [SerializeField] private float sunriseHour;
-    [SerializeField] private float sunsetHour;
-
-    private DateTime currentTime;
-    private TimeSpan dayTimeSpan;
-    private TimeSpan nightTimeSpan;
-
-    [SerializeField] private Color dayAmbientLight;
-    [SerializeField] private Color nightAmbientLight;
-    [SerializeField] private Color dawnAmbientLight;
-
-    [SerializeField] private AnimationCurve lightChangeCurve;
+    [SerializeField] private DateTime currentTime;
+    [SerializeField] private DateTime endDawnTime;
+    [SerializeField] private float initDawnTime;
 
 
-    private void Start()
+
+    private void Awake()
     {
-        currentTime = DateTime.Now.Date + TimeSpan.FromHours(initGameTime);
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(initDawnTime);
+        endDawnTime = DateTime.Now.Date + TimeSpan.FromHours(5f);
 
-        dayTimeSpan = TimeSpan.FromHours(sunriseHour);
-        nightTimeSpan = TimeSpan.FromHours(sunsetHour);
+        light = gameObject.GetComponent<Light>();
+
+        filter = light.color;
+        temperature = light.colorTemperature;
+        intensity = light.intensity;
     }
 
     private void Update()
     {
-        if (current == TimeOfDay.Night)
+        Debug.Log(currentTime.ToString());
+        if (current == TimeOfDay.Dawn && currentTime <= endDawnTime)
         {
             timeText.enabled = true;
 
-            UpdateTime();
-            RotateSun();
+            currentTime = currentTime.AddSeconds(Time.deltaTime * realTimeToGameTimeMultiplier);
+
+            if (timeText != null)
+            {
+                timeText.text = currentTime.ToString("HH:mm");
+            }
         }
         else
         {
             timeText.enabled = false;
-        }
-    }
-
-    private void UpdateTime()
-    {
-        currentTime = currentTime.AddSeconds(Time.deltaTime * realTimeToGameTimeMultiplier);
-
-        if (timeText != null)
-        {
-            timeText.text = currentTime.ToString("HH:mm");
-        }
+            current = TimeOfDay.Day;
+        }    
     }
 
     /// <summary>
-    /// case 1. 새벽 -> 낮 ///
-    /// case 2. 낮 -> 밤
+    /// Skip 
     /// </summary>
-    private void SkipToNextTime(TimeOfDay current)
+    public void DawnToDay()
     {
-        if      (current == TimeOfDay.Dawn)
-        {
-
-        }
-        else if (current == TimeOfDay.Day)
-        {
-
-        }
-        else
-        {
-            Debug.LogError("SkipToNextTime() - Not Found Current Time");
-            return;
-        }
-    }
-
-    /// <summary>
-    /// 밤 -> 새벽 
-    /// </summary>
-    private void DurationToNextTime(TimeOfDay current)
-    {
-        if (current == TimeOfDay.Night)
-        {
-            
-        }
-        else
-        {
-            Debug.LogError("DurationToNextTime() - Not Found Current Time");
-            return;
-        }
-    }
-
-
-    private void RotateSun()
-    {
-        Debug.Log("Rotate Sun");
-
-        float lightLerp;
-
-        if (currentTime.TimeOfDay > dayTimeSpan && currentTime.TimeOfDay < nightTimeSpan)
-        {
-            TimeSpan timeSinceSunrise = CalculateTime(dayTimeSpan, currentTime.TimeOfDay);
-            TimeSpan sunriseToSunsetDuration = CalculateTime(dayTimeSpan, nightTimeSpan);
-
-            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
-
-            lightLerp = Mathf.Lerp(200, 360, (float)percentage);
-            transform.rotation = Quaternion.AngleAxis(lightLerp, Vector3.right);
-
-            float dotProduct = Vector3.Dot(transform.forward, Vector3.down);
-            float intensityLerp = Mathf.Lerp(0, 1, lightChangeCurve.Evaluate(dotProduct));
-
-            GetComponent<Light>().intensity = intensityLerp;
-        }
-        else
-        {
-            SetDawnToDay();
-        }
-
-    }
-
-    private TimeSpan CalculateTime(TimeSpan fromTime, TimeSpan toTime)
-    {
-        TimeSpan difference = toTime - fromTime;
-
-        if (difference.TotalSeconds < 0.1f)
-        {
-            difference += TimeSpan.FromHours(24);
-        }
-
-        return difference;
-    }
-
-
-    #region Set Time Methods
-    public void SetDawnToDay()
-    {
-        Debug.Log("Set Dawn");
-
-        current = TimeOfDay.Dawn;
-        SkipToNextTime(current);
-    }
-
-    public void SetDayToNight()
-    {
-        Debug.LogWarning("Save All Data");
-        Debug.Log("Set Day");
-
+        Debug.Log("Dawn To Day");
         current = TimeOfDay.Day;
-        SkipToNextTime(current);
+
+        filter = new Color(1f, 1f, 0.8f);           // (원하는 색상 RGB 값) / 255 
+        temperature = 6500f;
+        intensity = 1f;
+
+        light.color = filter;
+        light.colorTemperature = temperature;
+        light.intensity = intensity;
     }
 
-    public void SetNightToDawn()
+    /// <summary>
+    /// Skip  
+    /// </summary>
+    public void DayToNight()
     {
-        Debug.Log("Set Night");
-
+        Debug.Log("Day To Night");
         current = TimeOfDay.Night;
-        DurationToNextTime(current);
-    }
-    #endregion
 
+        filter = new Color(0f, 0f, 0f);
+        temperature = 20000f;
+        intensity = 0f;
+
+        light.color = filter;
+        light.colorTemperature = temperature;
+        light.intensity = intensity;
+    }
+
+    /// <summary>
+    /// Duration  
+    /// </summary>
+    public void NightToDawn()
+    {
+        Debug.Log("Night To Dawn");
+        current = TimeOfDay.Dawn;
+
+        filter = new Color(1f, 0.7f, 1f);
+        temperature = 1500f;
+        intensity = 0.8f;
+
+        light.color = filter;
+        light.colorTemperature = temperature;
+        light.intensity = intensity;
+    }
 }
