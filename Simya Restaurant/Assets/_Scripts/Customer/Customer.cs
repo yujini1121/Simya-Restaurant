d4d77482bd7d4acd97c2e2ff895d2427d0dd5f6e
PChaebin <127742686+PChaebin@users.noreply.github.com>
2024-11-30 15:26:17 +0900

    24.11.30
    - 알사탕 / 유명인의 싸인

--- Simya Restaurant/Assets/_Scripts/Customer/Customer.cs
+++ Simya Restaurant/Assets/_Scripts/Customer/Customer.cs
@@ -37,6 +37,16 @@ public class Customer : MonoBehaviour
     [SerializeField] private string[] standardExitLines = { "수고하세요", "잘 먹었습니다" };
     [SerializeField] private string[] badExitLines = { "별점 테러할게요", "장사 이렇게 하지마세요", "퉤퉷, 여길 다신 오나 봐라" };
 
+    private Coroutine tipModifierCoroutine;
+
+    [SerializeField] private ItemAttribute item1;
+    [SerializeField] private ItemAttribute item2;
+
+    private enum items
+    {
+        Candy = 2001,
+        CelebrityAutographs
+    }
 
     private void Awake()
     {
@@ -91,6 +101,8 @@ public class Customer : MonoBehaviour
     /// </summary>
     public void ServeMenu()
     {
+        UsedItem(item2);
+
         if (foodRank == EFoodRank.None)
         {
             Debug.LogError("EFoodRank가 설정되지 않았습니다. 기본값 Bad로 설정됩니다.");
@@ -179,17 +191,21 @@ public class Customer : MonoBehaviour
         {
             case 1:
                 CustomerManager.instance.dangerFruitTartTotalPrice += finalPrice;
+                Debug.Log("dangerFruitTartTotalPrice : " + CustomerManager.instance.dangerFruitTartTotalPrice);
                 break;
             case 2:
                 CustomerManager.instance.flowerTeaTotalPrice += finalPrice;
+                Debug.Log("flowerTeaTotalPrice : " + CustomerManager.instance.flowerTeaTotalPrice);
                 break;
             case 3:
                 CustomerManager.instance.slimeJamBreadTotalPrice += finalPrice;
+                Debug.Log("slimeJamBreadTotalPrice : " + CustomerManager.instance.slimeJamBreadTotalPrice);
                 break;
         }
 
         CustomerManager.instance.totalPrice += finalPrice;
-        
+        Debug.Log("totalPrice : " + CustomerManager.instance.totalPrice);
+
 
         yield return new WaitForSeconds(3f);
 
@@ -211,5 +227,39 @@ public class Customer : MonoBehaviour
         };
 
         orderText.text = exitLines[Random.Range(0, exitLines.Length)];
+    }
+
+    private void RecoverHappines()
+    {
+        happiness = Mathf.Min(100, happiness + 20);
+        Debug.Log("happiness : " + happiness);
+    }
+
+    private IEnumerator InCreaseTipModifier(float duration)
+    {
+        tipModifier += 0.05f;
+        Debug.Log("tipModifier : " + tipModifier);
+
+        yield return new WaitForSeconds(duration);
+
+        tipModifier -= 0.05f;
+        Debug.Log("tipModifier : " + tipModifier);
+    }
+
+    private void UsedItem(ItemAttribute item)
+    {
+        switch((items)item.ItemID)
+        {
+            case items.Candy:
+                RecoverHappines();
+                break;
+            case items.CelebrityAutographs:
+                if(tipModifierCoroutine != null)
+                {
+                    StopCoroutine(tipModifierCoroutine);
+                }
+                tipModifierCoroutine = StartCoroutine(InCreaseTipModifier(60f));
+                break;
+        }
     }
 }
